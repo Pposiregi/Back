5c455ee5230324177230dd9cbc67571c241c1750
최경영 <yeong7098@naver.com>
2025-12-15 15:01:23 +0900

    Refactor : @Setter 제거

--- src/main/java/com/fitpet/server/alram/domain/entity/AlramMessage.java
+++ src/main/java/com/fitpet/server/alram/domain/entity/AlramMessage.java
@@ -16,14 +16,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "alram")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
