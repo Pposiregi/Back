2047728ac29f5bab4efb0314b7b75fd11b6042c3
barrackkk <kgmlwns904@gmail.com>
2025-12-17 11:36:56 +0900

    feat: gps_log 기록을 recorded_at 오름차순으로 가져오는 메서드 추가

--- src/main/java/com/fitpet/server/dailyworkout/domain/repository/GpsLogRepository.java
+++ src/main/java/com/fitpet/server/dailyworkout/domain/repository/GpsLogRepository.java
@@ -1,7 +1,11 @@
 package com.fitpet.server.dailyworkout.domain.repository;
 
 import com.fitpet.server.dailyworkout.domain.entity.GpsLog;
+import com.fitpet.server.dailyworkout.domain.entity.GpsSession;
+import java.util.List;
 
 public interface GpsLogRepository {
     GpsLog save(GpsLog gpsLog);
+
+    List<GpsLog> findByGpsSessionOrderByRecordedAtAsc(GpsSession gpsSession);
 }
