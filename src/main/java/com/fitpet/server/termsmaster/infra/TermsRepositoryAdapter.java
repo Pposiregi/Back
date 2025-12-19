21150e92d9b48acd3b5d518463b2297953de8969
최경영 <yeong7098@naver.com>
2025-12-19 12:55:00 +0900

    Feat : repository

---
+++ src/main/java/com/fitpet/server/termsmaster/infra/TermsRepositoryAdapter.java
@@ -0,0 +1,33 @@
+package com.fitpet.server.termsmaster.infra;
+
+import com.fitpet.server.termsmaster.domain.entity.Terms;
+import com.fitpet.server.termsmaster.domain.entity.TermsType;
+import com.fitpet.server.termsmaster.domain.repository.TermsRepository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class TermsRepositoryAdapter implements TermsRepository {
+
+    private final TermsJpaRepository jpaRepository;
+
+    @Override
+    public Terms save(Terms terms) {
+        return jpaRepository.save(terms);
+    }
+
+    @Override
+    public List<Terms> findAllActiveTerms(LocalDate date) {
+        return jpaRepository.findAllActiveTerms(date);
+    }
+
+    @Override
+    public Optional<Terms> findByCodeAndVersion(TermsType code, String version) {
+        //TODO : 메소드 추가
+        return Optional.empty();
+    }
+}
