8fb52a6fc6ad0d1ada27def1302f63d8bd6d558b
최경영 <yeong7098@naver.com>
2025-12-19 16:57:57 +0900

    Refactor : controller 두개를 하나로 합침

--- src/main/java/com/fitpet/server/shared/config/SecurityConfig.java
+++ src/main/java/com/fitpet/server/shared/config/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
             "/alram/**",
             "/pets/**",
             "/missions/**",
-            "/badges/**"
+            "/badges/**",
+            "/terms/**",
+
     };
 
     @Bean
--- src/main/java/com/fitpet/server/termsmaster/presentation/TermsController.java
+++ src/main/java/com/fitpet/server/termsmaster/presentation/TermsController.java
@@ -1,12 +1,19 @@
-package com.fitpet.server.termsmaster.presentation;
+package com.fitpet.server.termsmaster.presentation.controller;
 
+import com.fitpet.server.shared.annotation.AuthUser;
+import com.fitpet.server.termsmaster.application.dto.TermsAgreementCommand;
 import com.fitpet.server.termsmaster.application.dto.TermsDto;
+import com.fitpet.server.termsmaster.application.service.TermsAgreementService;
 import com.fitpet.server.termsmaster.application.service.TermsService;
+import com.fitpet.server.termsmaster.presentation.dto.TermsAgreementRequest;
 import com.fitpet.server.termsmaster.presentation.dto.TermsResponse;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TermsController {
 
     private final TermsService termsService;
+    private final TermsAgreementService termsAgreementService;
 
     @GetMapping
     public ResponseEntity<List<TermsResponse>> getTerms() {
@@ -26,5 +34,19 @@ public class TermsController {
                 .toList();
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/agreements")
+    public ResponseEntity<String> saveAgreements(
+            @AuthUser Long userId,
+            @RequestBody @Valid List<TermsAgreementRequest> requests
+    ) {
+        List<TermsAgreementCommand> commands = requests.stream()
+                .map(TermsAgreementRequest::toCommand)
+                .toList();
+
+        termsAgreementService.saveTermsAgreements(userId, commands);
+
+        return ResponseEntity.ok("약관 동의 내역이 저장되었습니다.");
     }
 }
