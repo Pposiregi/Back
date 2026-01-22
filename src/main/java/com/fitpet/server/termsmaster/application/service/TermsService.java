bb55af182d59bd505a42f80294efab73eeab7a02
최경영 <yeong7098@naver.com>
2026-01-22 17:20:09 +0900

    Refactor : Jackson 라이브러리가 ArrayList 타입을 읽을 수 있게 수정

--- src/main/java/com/fitpet/server/termsmaster/application/service/TermsService.java
+++ src/main/java/com/fitpet/server/termsmaster/application/service/TermsService.java
@@ -4,10 +4,10 @@ import com.fitpet.server.termsmaster.application.dto.TermsDto;
 import com.fitpet.server.termsmaster.domain.entity.Terms;
 import com.fitpet.server.termsmaster.domain.repository.TermsRepository;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,14 +19,13 @@ public class TermsService {
 
     private final TermsRepository termsRepository;
 
-    @Cacheable(value = "staticData", key = "'termsList'")
     public List<TermsDto> getActiveTerms() {
         log.info("[TermsService] DB에서 약관을 조회합니다.");
-        
+
         List<Terms> activeTerms = termsRepository.findAllActiveTerms(LocalDate.now());
 
-        return activeTerms.stream()
+        return new ArrayList<>(activeTerms.stream()
                 .map(TermsDto::from)
-                .toList();
+                .toList());
     }
 }
