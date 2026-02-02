27e61192b265fe3d815d3a658c80801bc515eb90
최경영 <yeong7098@naver.com>
2026-02-02 14:12:34 +0900

    Rafactor(terms) : TermsCreateRequest DTO에 유효성 추가

--- src/main/java/com/fitpet/server/termsmaster/presentation/dto/TermsCreateRequest.java
+++ src/main/java/com/fitpet/server/termsmaster/presentation/dto/TermsCreateRequest.java
@@ -1,10 +1,12 @@
 package com.fitpet.server.termsmaster.presentation.dto;
 
 import com.fitpet.server.termsmaster.domain.entity.TermsType;
+import jakarta.validation.constraints.NotBlank;
+import org.codehaus.commons.nullanalysis.NotNull;
 
 public record TermsCreateRequest(
-        TermsType code,
-        String content,
-        String version
+        @NotNull TermsType code,
+        @NotBlank String content,
+        @NotBlank String version
 ) {
 }
