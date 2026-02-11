bfe895049d22aba03b6fd35c7e8df3e10135b4cc
최경영 <yeong7098@naver.com>
2026-02-11 16:53:00 +0900

    Refactor : RankingFacade 남여 필터링에 맞게 수정

--- src/main/java/com/fitpet/server/ranking/application/facade/RankingFacade.java
+++ src/main/java/com/fitpet/server/ranking/application/facade/RankingFacade.java
@@ -3,6 +3,7 @@ package com.fitpet.server.ranking.application.facade;
 import com.fitpet.server.ranking.application.dto.RankingDto;
 import com.fitpet.server.ranking.application.dto.RankingSummaryDto;
 import com.fitpet.server.ranking.application.service.RankingService;
+import com.fitpet.server.ranking.domain.type.RankingFilter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,10 +14,11 @@ public class RankingFacade {
 
     private final RankingService rankingService;
 
-    public RankingSummaryDto getRankingSummary(Long userId) {
-        List<RankingDto> top10Dtos = rankingService.getTop10();
-        RankingDto myRankDto = rankingService.getMyRank(userId);
-        
+    public RankingSummaryDto getRankingSummary(Long userId, RankingFilter filter) {
+        List<RankingDto> top10Dtos = rankingService.getTop10(filter);
+
+        RankingDto myRankDto = rankingService.getMyRank(userId, filter);
+
         return RankingSummaryDto.of(top10Dtos, myRankDto);
     }
 
