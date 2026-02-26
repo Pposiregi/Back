6fd230a0d3fbb29e2f976e23e8c564922540d2f6
barrackkk <kgmlwns904@gmail.com>
2026-02-26 12:14:35 +0900

    feat: @Builder 추가

--- src/main/java/com/fitpet/server/user/presentation/dto/PetSummaryDto.java
+++ src/main/java/com/fitpet/server/user/presentation/dto/PetSummaryDto.java
@@ -2,7 +2,9 @@ package com.fitpet.server.user.presentation.dto;
 
 import com.fitpet.server.pet.domain.entity.PetExpression;
 import com.fitpet.server.pet.domain.entity.PetType;
+import lombok.Builder;
 
+@Builder
 public record PetSummaryDto(
         Long petId,
         String name,
